82d8700f88c595adaa0f05bc16e76a58dbaa24ae
sfratt <sfratt@users.noreply.github.com>
2022-02-21 12:54:47 -0500

    Accept instructions for robot from the command line

--- app/src/main/java/ca/concordia/encs/robot_motion/RobotMotion.java
+++ app/src/main/java/ca/concordia/encs/robot_motion/RobotMotion.java
@@ -1,76 +1,171 @@
 package ca.concordia.encs.robot_motion;
 
-import java.util.NoSuchElementException;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class RobotMotion {
     private Robot robot;
-    private Instruction instruction;
+    // private Instruction instruction;
     private Location location;
     private Pen pen;
     private Floor floor;
-    private int move;
+    private int moveSize;
+    private int floorSize;
+    private Scanner scanner;
+    private boolean gameActive;
 
     public RobotMotion() {
         location = new Location(new Point(0, 0), Direction.NORTH);
         pen = new Pen(false);
-        floor = new Floor(new Point(10, 10));
+        floor = new Floor(new Point(0, 0));
         robot = new Robot(location, pen, floor);
     }
 
-    public void run() throws NoSuchElementException {
-        while (true) {
-            System.out.println("Calling from robot motion");
+    public void run() {
+        gameActive = true;
+        while (gameActive) {
             String firstInput;
-            System.out.print("Enter command: ");
-            var scanner = new Scanner(System.in);
-            firstInput = scanner.next(); // reads input
-            System.out.println("First input test: " + firstInput);
-            scanner.close();
+            String secondInput;
+            String instruction = "nothing";
+            int instructionNumber = 0;
+
+            // clearConsole();
+            try {
+                System.out.print("Enter command: ");
+                scanner = new Scanner(System.in);
+                firstInput = scanner.next(); // reads input
+
+                if (firstInput.toUpperCase() != "") { // validates input
+                    instruction = firstInput.toUpperCase(); // input to uppercase and save
+                    if (instruction.equals("I") || instruction.equals("M")) {
+                        secondInput = scanner.next(); // read number
+                        if (isInt(secondInput)) { // validates number
+                            if (instruction.equals("I")) {
+                                if (Integer.parseInt(secondInput) > 0) {
+                                    instructionNumber = Integer.parseInt(secondInput);
+                                } else {
+                                    System.out.println("Please enter positive integer.");
+                                }
+                                setFloorSize(instructionNumber);
+                            } else {
+                                instructionNumber = Integer.parseInt(secondInput);
+                                setMove(instructionNumber);
+                            }
+                        } else {
+                            System.out.println("Please enter valid integer.");
+                        }
+                    }
+                } else {
+                    System.out.println("Please enter valid instruction.");
+                }
+
+                switch (instruction) {
+                    case "U":
+                        receiveInstruction('U');
+                        break;
+                    case "D":
+                        receiveInstruction('D');
+                        break;
+                    case "R":
+                        receiveInstruction('R');
+                        break;
+                    case "L":
+                        receiveInstruction('L');
+                        break;
+                    case "M":
+                        receiveInstruction('M');
+                        break;
+                    case "P":
+                        receiveInstruction('P');
+                        break;
+                    case "C":
+                        receiveInstruction('C');
+                        break;
+                    case "Q":
+                        receiveInstruction('Q');
+                        break;
+                    case "I":
+                        receiveInstruction('I');
+                        break;
+                }
+
+            } catch (Exception e) {
+                print(e.getMessage() + "\n");
+            }
         }
     }
 
     public void print(String output) {
-        System.out.println(output);
+        System.out.print(output);
+    }
+
+    public void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (IOException e) {
+            print(e.getMessage());
+        }
     }
 
     public void printFloor() {
         var floorGrid = robot.getFloor().getFloorGrid();
 
         for (int y = floorGrid.length - 1; y >= 0; y--) {
-            System.out.print(y);
+            System.out.format("%2d", y);
+            // print(String.valueOf(y));
             for (int x = 0; x < floorGrid.length; x++) {
                 if (floorGrid[x][y]) {
-                    System.out.print(" *");
+                    print(" *");
                 } else {
-                    System.out.print("  ");
+                    print("  ");
                 }
             }
-            System.out.print("\n");
+            print("\n");
         }
-        System.out.print(" ");
+        print(" ");
         for (int i = 0; i < floorGrid.length; i++) {
-            System.out.print(" " + i);
+            print(" " + i);
         }
-        System.out.print("\n");
+        print("\n");
     }
 
     public void printCurrentPosition() {
         var currentPosition = "Position: " + location.getX() + ", " + location.getY() + " - Pen: "
-                + (robot.getPenPosition() ? "down" : "up") + " - Facing: " + location.getDirection().toString();
+                + (robot.getPenPosition() ? "down" : "up") + " - Facing: " + location.getDirection().toString() + "\n";
         print(currentPosition);
     }
 
-    public Instruction getInstruction() {
-        return instruction;
-    }
+    // public Instruction getInstruction() {
+    // return instruction;
+    // }
 
-    public void setInstruction(char shortName) {
-        this.instruction = Instruction.getFromShortName(shortName);
-    }
+    // public void setInstruction(char shortName) {
+    // this.instruction = Instruction.getFromShortName(shortName);
+    // }
 
     public void setMove(int move) {
-        this.move = move;
+        this.moveSize = move;
+    }
+
+    public void setFloorSize(int floorSize) {
+        this.floorSize = floorSize;
+    }
+
+    public void initialize() {
+        location = new Location(new Point(0, 0), Direction.NORTH);
+        pen = new Pen(false);
+        floor = new Floor(new Point(floorSize, floorSize));
+        robot = new Robot(location, pen, floor);
+    }
+
+    public void quit() {
+        gameActive = false;
+        scanner.close();
     }
 
     public void receiveInstruction(char instruction) {
@@ -88,17 +183,33 @@ public class RobotMotion {
                 robot.turnRight();
                 break;
             case MOVE:
-                robot.move(move);
+                robot.move(moveSize);
                 break;
             case PRINT:
                 printFloor();
-                // print(instruction);
                 break;
             case CURRENT:
                 printCurrentPosition();
                 break;
-            default:
+            case QUIT:
+                quit();
                 break;
+            case INITIALIZE:
+                initialize();
+                break;
+            default:
+                // throw new IllegalArgumentException("Please enter a valid instruction");
+                break;
+        }
+    }
+
+    public static boolean isInt(String str) {
+        try {
+            @SuppressWarnings("unused")
+            int x = Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
--- app/src/test/java/ca/concordia/encs/robot_motion/RobotMotionTest.java
+++ app/src/test/java/ca/concordia/encs/robot_motion/RobotMotionTest.java
@@ -17,14 +17,25 @@ public class RobotMotionTest {
     @BeforeEach
     public void beforeEach() {
         robotMotion = new RobotMotion();
+        robotMotion.setFloorSize(10);
+        robotMotion.initialize();
         System.setOut(new PrintStream(outputStreamCaptor));
     }
 
     @Test
-    public void print_InvokePrintWithOutput_OutputCaptorSuccess() {
-        robotMotion.print("Hello Baeldung Readers!!");
+    public void printCurrentPosition_InvokePrintWithPenUp_CaptureOutputSuccessfully() {
+        robotMotion.printCurrentPosition();
+        assertEquals("Position: 0, 0 - Pen: up - Facing: NORTH", outputStreamCaptor.toString().trim());
+    }
 
-        assertEquals("Hello Baeldung Readers!!", outputStreamCaptor.toString().trim());
+    @Test
+    public void printCurrentPosition_InvokePrintWithPenDown_CaptureOutputSuccessfully() {
+        robotMotion.receiveInstruction('D');
+        robotMotion.receiveInstruction('R');
+        robotMotion.setMove(4);
+        robotMotion.receiveInstruction('M');
+        robotMotion.printCurrentPosition();
+        assertEquals("Position: 4, 0 - Pen: down - Facing: EAST", outputStreamCaptor.toString().trim());
     }
 
     @AfterEach
